14a10cfc8e935d7005a1c6ca04a88cca77ce03ec
Allen Xu <allxu@nvidia.com>
2021-12-07 18:48:19 +0800

    update for debug
    Signed-off-by: Allen Xu <allxu@nvidia.com>

--- examples/pca/scala/src/com/nvidia/spark/examples/pca/Main.scala
+++ examples/pca/scala/src/com/nvidia/spark/examples/pca/Main.scala
@@ -17,41 +17,70 @@
 
 package com.nvidia.spark.examples.pca
 
+import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.ml.feature.StandardScaler
+
 import org.apache.spark.ml.linalg._
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.functions.{array, avg, col, udf}
 
 object Main {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("PCA Example").getOrCreate()
     val dim = 2048
-    val rows = 100000
+    val rows = 1000
     val r = new scala.util.Random(0)
 
     // generate dummy data
-    val dataDf = spark.createDataFrame(
-      (0 until rows).map(_ => Tuple1(Array.fill(dim)(r.nextDouble)))).withColumnRenamed("_1", "feature")
+    var prepareDf = spark.createDataFrame(
+      (0 until rows).map(_ => Tuple1(Array.fill(dim)(r.nextDouble))))
+        .withColumnRenamed("_1", "feature")
+        .select((0 until dim).map(i => col("feature").getItem(i)): _*)
+
+    val avgValue = prepareDf.select(
+        (0 until dim).map("feature[" + _ + "]").map(col).map(avg): _*)
+          .first()
+
+    val inputCols = (0 until dim).map(i =>
+      (col("feature[" + i + "]") - avgValue.getDouble(i)).alias("feature_"+i)
+    )
+    prepareDf = prepareDf.select(inputCols:_*)
+
+
+    val dataDf = prepareDf.withColumn("feature",array(prepareDf.columns.map(col):_*))
+
+    dataDf.write.parquet("~/tmp/dataDf.parquet")
+    val readDataDf = spark.read.parquet("~/tmp/dataDf.parquet")
+
+
+
     // use udf to meet ML algo input requirement: Vector input
-    val convertToVector = udf((array: Seq[Float]) => {
+    val convertToVector = udf((array: Seq[Double]) => {
       Vectors.dense(array.map(_.toDouble).toArray)
     })
 
-    val vectorDf = dataDf.withColumn("feature_vec", convertToVector(col("feature"))).repartition(1)
+    val vectorDf = readDataDf.withColumn("feature_vec", convertToVector(col("feature"))).repartition(1)
 
-    val pcaGpu = new com.nvidia.spark.ml.feature.PCA().setInputCol("feature_vec").setTransformInputCol("feature").setOutputCol("pca_features").setK(3).setUseGemm(true).setUseCuSolverSVD(true)
+    val pcaGpu = new com.nvidia.spark.ml.feature.PCA().setInputCol("feature").setOutputCol("pca_features").setK(3).setGpuId(0)
     // GPU train
     val gpuStart = System.currentTimeMillis()
-    val pcaModelGpu = pcaGpu.fit(vectorDf)
+    val pcaModelGpu = pcaGpu.fit(readDataDf)
     val gpuEnd = System.currentTimeMillis()
 
+    println("=======  GPU pc: =========")
+    print(pcaModelGpu.pc)
+
     // use original Spark ML PCA class
-    val pcaCpu = new org.apache.spark.ml.feature.PCA().setInputCol("feature").setOutputCol("pca_features").setK(3)
+    val pcaCpu = new org.apache.spark.ml.feature.PCA().setInputCol("feature_vec").setOutputCol("pca_features").setK(3)
 
     // CPU train
     
     val cpuStart = System.currentTimeMillis()
     val pcaModelCpu = pcaCpu.fit(vectorDf)
     val cpuEnd = System.currentTimeMillis()
+
+    println("=======  CPU pc: =========")
+    print(pcaModelCpu.pc)
 
     
     println("GPU training: ")
@@ -60,7 +89,7 @@ object Main {
     println( (cpuEnd - cpuStart) / 1000 + " seconds")
 
     // transform
-    pcaModelGpu.transform(vectorDf).select("pca_features").show(false)
+    pcaModelGpu.transform(readDataDf).select("pca_features").show(false)
     pcaModelCpu.transform(vectorDf).select("pca_features").show(false)
   }
 }
